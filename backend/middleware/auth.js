26aef82b48a59155ed2998f625af032265761b4f
Giorthy Lopez <giorthy.lopez@inacapmail.cl>
2025-12-18 11:32:17 -0300

    middleware added - bcrypt and jwt installed

---
+++ backend/middleware/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+
+const verificarAdmin = ( req, res, next) => {
+    try{
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith('Bearer ')){
+            return res.status(401).json({ 
+                error: 'Token no proporcionado',
+                code : 'NO_TOKEN'
+            })
+            
+        }
+
+        req.user = decoded;
+        next();
+    } catch (error){
+        if(error.name === 'TokenExpiredError') {
+            return  res.status(401).json({
+                error: 'Token expirado',
+                code: 'TOKEN_EXPIRED'
+            });
+        }
+        
+        return res.status(401).json({
+            error: 'Token inválido',
+            code : 'INVALID_TOKEN'
+        });
+    }
+};
+
+module.exports = { verificarAdmin};
